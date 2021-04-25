808a2529281d6b3ab221a9d2d40a33c4120a7501
William Droz <william.droz.ch@gmail.com>
2021-04-25 17:26:16 +0200

    add function to search url from a file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
-use std::path::Path;
+use std::{future::Future, path::Path};
+use std::fs;
+use regex::Regex;
 
 use glob::{glob, Pattern, PatternError};
+
+async fn read_urls_from_file(file: String) -> Option<Vec<String>> {
+    let regex = Regex::new(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)").unwrap();
+    let contents = fs::read_to_string(file.clone());
+    match contents {
+        Ok(lines) => {
+            let mut urls = Vec::new();
+            for caps in regex.captures_iter(&lines) {
+                for cap in caps.iter() {
+                    if let Some(capture) = cap {
+                        let url = capture.as_str().to_string();
+                        urls.push(url);
+                    }
+                }
+            }
+            Some(urls)
+        }
+        Err(_) => None,
+    }    
+}
 
 fn get_files(glob_expr: &str) -> Result<Option<Vec<String>>, PatternError> {
     let glob_res = glob(glob_expr);
