21eec7390dc4627ba9e073d2203266512fd14dfa
William Droz <william.droz.ch@gmail.com>
2021-04-25 21:57:13 +0200

    (WIP) bunch of async functions

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use std::{future::Future, path::Path};
 use std::fs;
 use regex::Regex;
 // Or maybe use Stream?
-use futures::stream::{FuturesUnordered, StreamExt};
+use futures::{FutureExt, stream::{FuturesUnordered, StreamExt}};
+use futures::executor::block_on;
+use reqwest;
 
 use glob::{glob, Pattern, PatternError};
 
 async fn read_urls_from_file(file: String) -> Option<Vec<String>> {
-    let regex = Regex::new(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)").unwrap();
+    let regex = Regex::new(r#"https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"#).unwrap();
     let contents = fs::read_to_string(file.clone());
     match contents {
         Ok(lines) => {
@@ -16,7 +18,10 @@ async fn read_urls_from_file(file: String) -> Option<Vec<String>> {
                 for cap in caps.iter() {
                     if let Some(capture) = cap {
                         let url = capture.as_str().to_string();
-                        urls.push(url);
+                        if !url.is_empty() {
+                            println!("pushing {}", url);
+                            urls.push(url);
+                        }
                     }
                 }
             }
@@ -26,11 +31,35 @@ async fn read_urls_from_file(file: String) -> Option<Vec<String>> {
     }    
 }
 
-fn process_files(files: Vec<String>) {
+async fn check_url(url: String) -> bool {
+    let client = reqwest::Client::new();
+    let res =client.head(url).send().await;
+    match res {
+        Ok(_) => true,
+        Err(_) => false
+    }
+}
+
+async fn print_result(r: bool) {
+    println!("result is {}", r);
+}
+
+async fn handle_urls(maybe_urls: Option<Vec<String>>) {
+    if let Some(urls) = maybe_urls {
+        let mut workers = FuturesUnordered::new();
+        for url in urls{
+            workers.push(check_url(url))
+        }
+        workers.for_each_concurrent(20, print_result).await;
+    }
+}
+
+async fn process_files(files: Vec<String>) {
     let mut workers = FuturesUnordered::new();
     for file in files {
         workers.push(read_urls_from_file(file.clone()));
     }
+    workers.for_each_concurrent(20, handle_urls).await;
 }
 
 fn get_files(glob_expr: &str) -> Result<Option<Vec<String>>, PatternError> {
@@ -52,6 +81,9 @@ fn get_files(glob_expr: &str) -> Result<Option<Vec<String>>, PatternError> {
 }
 
 fn main() {
-    println!("Hello, world!");
-    get_files("*.md");
+    let maybe_files = get_files("*.md").expect("error with the pattern");
+    if let Some(files) = maybe_files {
+        block_on(process_files(files));
+    }
+    
 }
