af4cc7ee5ca34819b885477be2c321716b332ae5
wdroz <william.droz.ch@gmail.com>
2022-06-27 20:40:53 +0200

    chore: remove comments

--- src/checkurls/mod.rs
+++ src/checkurls/mod.rs
@@ -1,13 +1,11 @@
 use std::path::Path;
 use tokio::fs::File;
-use tokio::io::AsyncReadExt; // for read_to_end()
+use tokio::io::AsyncReadExt;
 
 use ignore::{DirEntry, Walk};
 
 pub async fn get_files(path: String) {
     for result in Walk::new(path) {
-        // Each item yielded by the iterator is either a directory entry or an
-        // error, so either print the path or the error.
         match result {
             Ok(entry) => process_entry(&entry).await,
             Err(err) => println!("ERROR: {}", err),
