b739ca1db6e0491bc2552354d75ec869c279001c
wdroz <william.droz.ch@gmail.com>
2022-06-27 20:23:05 +0200

    feat: function that check if it's a file

--- src/checkurls/mod.rs
+++ src/checkurls/mod.rs
@@ -1,11 +1,20 @@
-use ignore::Walk;
+use ignore::{DirEntry, Walk};
+
 pub async fn get_files(path: String) {
     for result in Walk::new(path) {
         // Each item yielded by the iterator is either a directory entry or an
         // error, so either print the path or the error.
         match result {
-            Ok(entry) => println!("{}", entry.path().display()),
+            Ok(entry) => process_entry(&entry).await,
             Err(err) => println!("ERROR: {}", err),
+        }
+    }
+}
+
+async fn process_entry(entry: &DirEntry) {
+    if let Some(file_type) = entry.file_type() {
+        if !file_type.is_dir() {
+            println!("FILE {}", entry.path().display());
         }
     }
 }
