610470e8765171f61c4fbb27a49b13ca23ee3373
wdroz <william.droz.ch@gmail.com>
2022-06-27 20:40:15 +0200

    feat: function that read and print the len of files

--- src/checkurls/mod.rs
+++ src/checkurls/mod.rs
@@ -1,3 +1,7 @@
+use std::path::Path;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt; // for read_to_end()
+
 use ignore::{DirEntry, Walk};
 
 pub async fn get_files(path: String) {
@@ -15,6 +19,16 @@ async fn process_entry(entry: &DirEntry) {
     if let Some(file_type) = entry.file_type() {
         if !file_type.is_dir() {
             println!("FILE {}", entry.path().display());
+            check_urls(entry.path().as_ref()).await;
+        }
+    }
+}
+
+async fn check_urls(path: &Path) {
+    if let Ok(mut file) = File::open(path).await {
+        let mut contents = vec![];
+        if let Ok(_) = file.read_to_end(&mut contents).await {
+            println!("len = {}", contents.len());
         }
     }
 }
